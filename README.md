a5a2076dabbbf8a30d9b01ad0f0a7755831aa0a1
Srikant Kalaputapu <skalaputapu@gmail.com>
2017-07-11 11:30:27 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+A DBUTILITY class created for jdbc projects. Currently handles sql as user input, which is unsafe and should be changed later for better security.
+
+Srikant Kumar Kalaputapu
--- src/Utility/UtilityApp.java
+++ src/Utility/UtilityApp.java
@@ -11,27 +11,27 @@ public class UtilityApp {
 		
 		
 		
-//		ArrayList<LinkedHashMap<String, String>> answer = new ArrayList<>();
-//		try {
-//			answer = DBUtility.select("select * from customers where rownum <= 5");
-//		} catch (IOException e2) {
-//			// TODO Auto-generated catch block
-//			e2.printStackTrace();
-//		}
+		ArrayList<LinkedHashMap<String, String>> answer = new ArrayList<>();
+		try {
+			answer = DBUtility.select("select * from customers where rownum <= 5");
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 		System.out.println("-------------------------");
 		
 		//PROBLEM with order of columns since keyset of hashmap
 		///use linked hashmap
 		//can simplify
-//		for(HashMap<String, String> e : answer){
-//			
-//			for(String key : e.keySet()){
-//				System.out.println(key + ": " +e.get(key));
-//			}
-//			
-//			System.out.println("-------------------------");
-//		}
-//		
+		for(HashMap<String, String> e : answer){
+			
+			for(String key : e.keySet()){
+				System.out.println(key + ": " +e.get(key));
+			}
+			
+			System.out.println("-------------------------");
+		}
+		
 		try {
 			System.out.println("Rows affected: " + DBUtility.update("update cities set city = 'Limbo' where citiesid < 395") + "");
 		} catch (IOException e1) {
